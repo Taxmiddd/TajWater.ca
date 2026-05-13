305d63323fbee07eed00b90323bed16d5e208097
Tahmid Ashfaque <ashfaquet874@gmail.com>
2026-05-14 02:02:04 +0600

    fix: move plan setup page from /plan/[token] to /pay/plan/[token]

--- app/admin/subscriptions/page.tsx
+++ app/admin/subscriptions/page.tsx
@@ -164,7 +164,7 @@ export default function AdminSubscriptionsPage() {
   }
 
   const planLink = createdToken
-    ? `${typeof window !== 'undefined' ? window.location.origin : ''}/plan/${createdToken}`
+    ? `${typeof window !== 'undefined' ? window.location.origin : ''}/pay/plan/${createdToken}`
     : ''
 
   const copyLink = () => {
